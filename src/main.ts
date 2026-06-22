5eaf764e4f5614bcdfff63a5159b237560497f15
Adityaraj002 <adityaraj99106@gmail.com>
2026-06-23 02:33:23 +0530

    implemented logger interceptor which help let know how much time and route takes like Post auth/login 300ms

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common'
 import { HttpExceptionFilters } from './common/filters/http-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(
+    new LoggingInterceptor()
+  )
   app.useGlobalFilters(
     new HttpExceptionFilters()
   )
